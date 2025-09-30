23ec7eb7e3a4d8068c5b71b87a9e2ea2533b61ca
Skubivi <altair100001@gmail.com>
2025-09-30 20:56:18 +0300

    small changes

--- src/pages/mechanic-page/mechanic-page.tsx
+++ src/pages/mechanic-page/mechanic-page.tsx
@@ -2,17 +2,20 @@ import styles from './style.module.scss'
 
 import logo from '../../assets/logo-mini.png'
 import { Outlet } from 'react-router-dom'
-import { useGetMyFilialQuery } from '../../services/api/filial'
+import { useGetFilialsQuery } from '../../services/api/filial'
 import { useEffect, useState } from 'react'
-import { setFilialToLocalStorage } from '../../services/utils/helper-functions/filial'
+import { getFilialFromLocalStorage, setFilialToLocalStorage } from '../../services/utils/helper-functions/filial'
 
 import BurgerSvg from '../../assets/pages/mechanic-page/burger.svg?react'
 import Sidebar from './components/sidebar/sidebar'
 
 const MechanicPage = () => {
-    const { data: filial, isSuccess } = useGetMyFilialQuery()
+    const filial = getFilialFromLocalStorage()
+    const { data: filials, isSuccess } = useGetFilialsQuery()
     useEffect(() => {
-        if (filial) setFilialToLocalStorage(filial.id)
+        if (filial === null) {
+            if (filials && filials.data.length > 0) setFilialToLocalStorage(filials.data[0].id)
+        }
     }, [isSuccess])
     const [openSideBar, setOpenSideBar] = useState(false)
 
